6f70f3a83ac74c52b0ea06042f1f37aca3314b8e
ludengdeng-for-who <2649761276@qq.com>
2020-04-14 16:02:31 +0800

    我的页面

--- src/views/mine/Mine.vue
+++ src/views/mine/Mine.vue
@@ -16,24 +16,144 @@
       </div>
     </div>
     <div class="wallet">
-        <div class="wallet-top">
-            <div></div>
+      <div class="wallet-top">
+        <Item :bgc="'#ff7043'">
+          <template v-slot:left>
+            <i class="icon iconfont icon-qianbao"></i>
+          </template>
+          <template v-slot:center>
             <div>我的钱包</div>
+          </template>
+        </Item>
+      </div>
+      <div class="wallet-bottom">
+        <div v-for="item in list" :key="item.id" class="item">
+          <div class="item-money">{{ item.money }}</div>
+          <div class="item-title">{{ item.title }}</div>
         </div>
+      </div>
+    </div>
+    <div class="other">
+      <div class="other-item">
+        <Item :bgc="'#ff7043'">
+          <template v-slot:left>
+            <i class="icon iconfont icon-icon-"></i>
+          </template>
+          <template v-slot:center>
+            <div>我的钱包</div>
+          </template>
+          <template v-slot:right>
+            <i class="icon iconfont icon-qianjin"></i>
+          </template>
+        </Item>
+      </div>
+      <div class="other-item">
+        <Item :bgc="'#ffa21f'">
+          <template v-slot:left>
+            <i class="icon iconfont icon-qianbao"></i>
+          </template>
+          <template v-slot:center>
+            <div>我的退款</div>
+          </template>
+          <template v-slot:right>
+            <i class="icon iconfont icon-qianjin"></i>
+          </template>
+        </Item>
+      </div>
+      <div class="other-item">
+        <Item :bgc="'#ffad3b'">
+          <template v-slot:left>
+            <i class="icon iconfont icon-qianbao"></i>
+          </template>
+          <template v-slot:center>
+            <div>我的发票</div>
+          </template>
+          <template v-slot:right>
+            <i class="icon iconfont icon-qianjin"></i>
+          </template>
+        </Item>
+      </div>
+      <div class="other-item">
+        <Item :bgc="'#ffa21f'">
+          <template v-slot:left>
+            <i class="icon iconfont icon-gonglve"></i>
+          </template>
+          <template v-slot:center>
+            <div>我的攻略</div>
+          </template>
+          <template v-slot:right>
+            <i class="icon iconfont icon-qianjin"></i>
+          </template>
+        </Item>
+      </div>
+    </div>
+    <div class="about">
+      <div class="other-item">
+        <Item :bgc="'#90a4ae'">
+          <template v-slot:left>
+            <i class="icon iconfont icon-xinxi"></i>
+          </template>
+          <template v-slot:center>
+            <div>意见反馈y</div>
+          </template>
+          <template v-slot:right>
+            <i class="icon iconfont icon-qianjin"></i>
+          </template>
+        </Item>
+      </div>
+      <div class="other-item">
+        <Item :bgc="'#90a4ae'">
+          <template v-slot:left>
+            <i class="icon iconfont icon-guanyu"></i>
+          </template>
+          <template v-slot:center>
+            <div>关于我们</div>
+          </template>
+          <template v-slot:right>
+            <i class="icon iconfont icon-qianjin"></i>
+          </template>
+        </Item>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import Item from "./components/Item";
+export default {
+  components: {
+    Item
+  },
+  data() {
+    return {
+      list: [
+        {
+          money: 999999999,
+          id: 1,
+          title: "账户余额"
+        },
+        {
+          money: 13215,
+          id: 2,
+          title: "星券"
+        },
+        {
+          money: 12,
+          id: 3,
+          title: "银行卡"
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style lang="less" scoped>
 @import url("~@/assets/css/colors.less");
-.mine{
-    width: 100vw;
-    height: 100vh;
-    background-color: #ebeced;
+.mine {
+  width: 100vw;
+  height: 100vh;
+  background-color: #ebeced;
 }
 .top {
   height: 6.4rem /* 160/25 */;
@@ -63,11 +183,37 @@ export default {};
       width: 2.6rem /* 65/25 */;
       height: 2.6rem;
     }
-    .login{
-        color: #fff;
-        margin-top: 6px;
-        font-size: 0.56rem /* 14/25 */;
+    .login {
+      color: #fff;
+      margin-top: 6px;
+      font-size: 0.56rem /* 14/25 */;
     }
   }
+}
+.wallet {
+  margin: 0.4rem /* 10/25 */ 0;
+  background-color: #fff;
+  .wallet-bottom {
+    border-top: 1px solid #eee;
+    display: flex;
+    justify-content: space-around;
+    padding: 0.8rem /* 20/25 */ 0.4rem /* 10/25 */;
+    .item {
+      font-size: 0.56rem /* 14/25 */;
+      text-align: center;
+      .item-title {
+        color: #888;
+        font-size: 12px;
+        margin-top: 0.4rem; /* 10/25 */
+      }
+    }
+  }
+}
+[class$=item] {
+    border-bottom: 1px solid #eee;
+  }
+.other{
+  margin-bottom: 0.4rem /* 10/25 */;
+  
 }
 </style>
---
+++ src/views/mine/components/Item.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="item">
+          <div class="left" :style="{backgroundColor:bgc}">
+              <slot name="left"></slot>
+          </div>
+          <div class="center">
+              <slot name="center" ></slot>
+          </div>
+          <div class="right">
+            <slot name="right"></slot>
+          </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"item",
+    props:{
+        bgc:{
+            type:String,
+            default :''
+        },
+        icon:{
+            type:String,
+            default:''
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.item{
+    background-color: #fff;
+    display: flex;
+    align-items: center;
+    padding: 0.32rem /* 8/25 */ ;
+    .center{
+        font-size: 0.56rem /* 14/25 */;
+        flex: 1;
+    }
+    .left{
+        width: 30px;
+        height: 30px;
+        border-radius: 10px;
+        margin-right: 0.4rem /* 10/25 */;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: #fff;
+    }
+}
+</style>
